557a18a034b50916928373be1ef221260f2a738a
virh <virgil.hong@aliyun.com>
2019-07-23 17:30:02 +0800

    init simple project

---
+++ simple/src/main/java/virh/sense/trade/domain/BackOrder.java
@@ -0,0 +1,8 @@
+package virh.sense.trade.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class BackOrder extends Order {
+
+}
---
+++ simple/src/main/java/virh/sense/trade/domain/SaleOrder.java
@@ -0,0 +1,9 @@
+package virh.sense.trade.domain;
+
+import javax.persistence.Entity;
+
+@Entity
+public class SaleOrder extends Order {
+
+	
+}
